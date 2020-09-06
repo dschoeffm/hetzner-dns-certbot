b921f17bac8c1416745f167005b3553c924b4369
Dominik Schoeffmann <dominik_schoeffmann@posteo.net>
2020-09-06 08:42:34 +0200

    revert commits which broke subdomains

--- certbot-hetzner-auth.sh
+++ certbot-hetzner-auth.sh
@@ -12,18 +12,5 @@ curl -X "POST" "https://dns.hetzner.com/api/v1/records" \
      -H "Auth-API-Token: ${token}" \
      -d "{ \"value\": \"${CERTBOT_VALIDATION}\", \"ttl\": 86400, \"type\": \"TXT\", \"name\": \"_acme-challenge\", \"zone_id\": \"${zone_id}\" }" > /dev/null 2>/dev/null
 
-
-POLLING_DNS=`dig +short NS ${CERTBOT_DOMAIN} | awk 'NR==1' | sed 's/.$//'`
-POLLING_INTERVAL=2
-POLLING_TIMEOUT=100
-echo "Waiting for TXT record _acme-challenge.${CERTBOT_DOMAIN} with value \"${CERTBOT_VALIDATION}\" in NS ${POLLING_DNS}..."
-i=0
-until dig @${POLLING_DNS} -t txt _acme-challenge.${CERTBOT_DOMAIN} | grep ${CERTBOT_VALIDATION}
-do
-  ((i=i+POLLING_INTERVAL))
-  if [[ $i -gt $POLLING_TIMEOUT ]]; then
-    echo "Timed out waiting for TXT record" 1>&2;
-    exit 1
-  fi
-	sleep $POLLING_INTERVAL
-done
+# just make sure we sleep for a while (this should be a dig poll loop)
+sleep 30
